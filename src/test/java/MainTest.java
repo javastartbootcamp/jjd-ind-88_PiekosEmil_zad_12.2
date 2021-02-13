800a3c109dc373e9fe41fe6d3bfe869610f74a11
Marcin Kunert <the.martines@gmail.com>
2021-02-13 20:07:29 +0100

    Refactoring testów

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,102 +1,207 @@
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
-import java.nio.file.CopyOption;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
+import java.util.HashSet;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MainTest {
 
+    @Nested
+    class MultipleEmployees {
 
-    @Test
-    void shouldCalculateStats() throws IOException {
-        File file = new File("employees.csv");
+        @BeforeEach
+        public void init() throws IOException  {
+            File file = new File("employees.csv");
 
-        FileWriter fileWriter = new FileWriter(file);
-        fileWriter.append("Jan;Kowalski;123456789;Management;6000\n");
-        fileWriter.append("Aneta;Zawadzka;432156789;IT;5200\n");
-        fileWriter.append("Karol;Kowalewski;5412328948;IT;5200\n");
-        fileWriter.append("Jan;Zarzecki;678628373674;Support;4800\n");
-        fileWriter.close();
-
-        // when
-        Main.main(new String[]{});
-
-        // then
-        File statsFile = new File("stats.txt");
-        assertThat(statsFile).exists();
-
-        String fileContent = Files.readString(statsFile.toPath());
-
-        assertThat(fileContent).contains("Średnia wypłata: 5300");
-        assertThat(fileContent).contains("Minimalna wypłata: 4800");
-        assertThat(fileContent).contains("Maksymalna wypłata: 6000");
-        assertThat(fileContent).contains("Liczba pracowników IT: 2");
-        assertThat(fileContent).contains("Liczba pracowników Management: 1");
-        assertThat(fileContent).contains("Liczba pracowników Support: 1");
-    }
-
-    @Test
-    void shouldCalculateStatsOnlyOneEmployee() throws IOException {
-        File file = new File("employees.csv");
-
-        FileWriter fileWriter = new FileWriter(file);
-        fileWriter.append("Jan;Kowalski;123456789;IT;6000\n");
-        fileWriter.close();
-
-        // when
-        Main.main(new String[]{});
-
-        // then
-        File statsFile = new File("stats.txt");
-        assertThat(statsFile).exists();
-        String fileContent = Files.readString(statsFile.toPath());
-
-        assertThat(fileContent).contains("Średnia wypłata: 6000");
-        assertThat(fileContent).contains("Minimalna wypłata: 6000");
-        assertThat(fileContent).contains("Maksymalna wypłata: 6000");
-        assertThat(fileContent).contains("Liczba pracowników IT: 1");
-    }
-
-
-    @BeforeEach
-    public void init() throws IOException {
-        createBackup();
-    }
-
-    @AfterEach
-    public void cleanup() {
-        try {
-            restoreBackup();
-        } catch (Exception e) {
-            System.out.println("Failed restoring backup");
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.append("Jan;Kowalski;123456789;Management;6000\n");
+            fileWriter.append("Aneta;Zawadzka;432156789;IT;5200\n");
+            fileWriter.append("Karol;Kowalewski;5412328948;IT;5200\n");
+            fileWriter.append("Jan;Zarzecki;678628373674;Support;4800\n");
+            fileWriter.close();
         }
-        new File("stats.txt").delete();
-    }
 
+        @Test
+        void noDuplicatedLines() throws IOException {
+            // when
+            Main.main(new String[]{});
 
-    private void createBackup() throws IOException {
-        File orgFile = new File("employees.csv");
-        if (orgFile.exists()) {
-            File backup = new File("backup.csv");
-            backup.delete();
-            Files.copy(orgFile.toPath(), backup.toPath());
+            // then
+            File statsFile = new File("stats.txt");
+            assertThat(statsFile).exists();
+            List<String> allLines = Files.readAllLines(statsFile.toPath());
+
+            HashSet<String> allLinesWithoutDuplicates = new HashSet<>(allLines);
+
+            assertThat(allLines.size()).as("Plik wynikowy zawiera duplikaty!").isEqualTo(allLinesWithoutDuplicates.size());
         }
-    }
 
-    private void restoreBackup() throws IOException {
-        File orgFile = new File("employees.csv");
-        File backup = new File("backup.csv");
-        orgFile.delete();
-        if (backup.exists()) {
-            Files.copy(backup.toPath(), orgFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-            backup.delete();
+        @Test
+        void avgSalary() throws IOException {
+            // when
+            Main.main(new String[]{});
+
+            // then
+            File statsFile = new File("stats.txt");
+            assertThat(statsFile).exists();
+            String fileContent = Files.readString(statsFile.toPath());
+
+            assertThat(fileContent).contains("Średnia wypłata: 5300");
         }
+
+        @Test
+        void minSalary() throws IOException {
+            // when
+            Main.main(new String[]{});
+
+            // then
+            File statsFile = new File("stats.txt");
+            assertThat(statsFile).exists();
+            String fileContent = Files.readString(statsFile.toPath());
+
+            assertThat(fileContent).contains("Minimalna wypłata: 4800");
+        }
+
+        @Test
+        void maxSalary() throws IOException {
+            // when
+            Main.main(new String[]{});
+
+            // then
+            File statsFile = new File("stats.txt");
+            assertThat(statsFile).exists();
+            String fileContent = Files.readString(statsFile.toPath());
+
+            assertThat(fileContent).contains("Maksymalna wypłata: 6000");
+        }
+
+        @Test
+        void iTDepartmentEmployeeCount() throws IOException {
+            // when
+            Main.main(new String[]{});
+
+            // then
+            File statsFile = new File("stats.txt");
+            assertThat(statsFile).exists();
+            String fileContent = Files.readString(statsFile.toPath());
+
+            assertThat(fileContent).contains("Liczba pracowników IT: 2");
+        }
+
+        @Test
+        void managementDepartmentEmployeeCount() throws IOException {
+            // when
+            Main.main(new String[]{});
+
+            // then
+            File statsFile = new File("stats.txt");
+            assertThat(statsFile).exists();
+            String fileContent = Files.readString(statsFile.toPath());
+
+            assertThat(fileContent).contains("Liczba pracowników Management: 1");
+        }
+
+        @Test
+        void supportDepartmentEmployeeCount() throws IOException {
+            // when
+            Main.main(new String[]{});
+
+            // then
+            File statsFile = new File("stats.txt");
+            assertThat(statsFile).exists();
+            String fileContent = Files.readString(statsFile.toPath());
+
+            assertThat(fileContent).contains("Liczba pracowników Support: 1");
+        }
+
     }
 
+    @Nested
+    class OnlyOneEmployeeInHRDepartment {
+
+        @BeforeEach
+        public void init() throws IOException  {
+            File file = new File("employees.csv");
+
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.append("Jan;Kowalski;123456789;HR;6000\n");
+            fileWriter.close();
+        }
+
+        @Test
+        void noDuplicatedLines() throws IOException {
+            // when
+            Main.main(new String[]{});
+
+            // then
+            File statsFile = new File("stats.txt");
+            assertThat(statsFile).exists();
+            List<String> allLines = Files.readAllLines(statsFile.toPath());
+
+            HashSet<String> allLinesWithoutDuplicates = new HashSet<>(allLines);
+
+            assertThat(allLines.size()).as("Plik wynikowy zawiera duplikaty!").isEqualTo(allLinesWithoutDuplicates.size());
+        }
+
+        @Test
+        void avgSalary() throws IOException {
+            // when
+            Main.main(new String[]{});
+
+            // then
+            File statsFile = new File("stats.txt");
+            assertThat(statsFile).exists();
+            String fileContent = Files.readString(statsFile.toPath());
+
+            assertThat(fileContent).contains("Średnia wypłata: 6000");
+        }
+
+        @Test
+        void minSalary() throws IOException {
+            // when
+            Main.main(new String[]{});
+
+            // then
+            File statsFile = new File("stats.txt");
+            assertThat(statsFile).exists();
+            String fileContent = Files.readString(statsFile.toPath());
+
+            assertThat(fileContent).contains("Minimalna wypłata: 6000");
+        }
+
+        @Test
+        void maxSalary() throws IOException {
+            // when
+            Main.main(new String[]{});
+
+            // then
+            File statsFile = new File("stats.txt");
+            assertThat(statsFile).exists();
+            String fileContent = Files.readString(statsFile.toPath());
+
+            assertThat(fileContent).contains("Maksymalna wypłata: 6000");
+        }
+
+        @Test
+        void departmentEmployeeCount() throws IOException {
+            // when
+            Main.main(new String[]{});
+
+            // then
+            File statsFile = new File("stats.txt");
+            assertThat(statsFile).exists();
+            String fileContent = Files.readString(statsFile.toPath());
+
+            assertThat(fileContent).contains("Liczba pracowników HR: 1");
+        }
+
+    }
 
 }
