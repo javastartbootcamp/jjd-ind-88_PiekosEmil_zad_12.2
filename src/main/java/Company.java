c749f423c6435d3df6435dbcf46c9e85f90d7842
Emil Piękoś <piekosemil@gmail.com>
2024-06-14 18:41:43 +0200

    Poprawki.

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -1,25 +1,24 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Company {
     private static final int MAX_EMPLOYEES = 100;
-    Employee[] employees = new Employee[MAX_EMPLOYEES];
 
-    public void readEmployees() {
+    public static Employee[] readEmployees() {
+        Employee[] employees = new Employee[MAX_EMPLOYEES];
         int index = 0;
         String firstName = null;
         String lastName = null;
         String id = null;
         String department = null;
         int salary = 0;
-        try {
-            File sourceFile = new File("employees.csv");
-            FileReader fileReader = new FileReader(sourceFile);
-            Scanner scan = new Scanner(fileReader).useDelimiter(";");
-            while (scan.hasNext() && scan != null) {
+
+        File sourceFile = new File("employees.csv");
+        try (FileReader fileReader = new FileReader(sourceFile);
+             Scanner scan = new Scanner(fileReader).useDelimiter(";");) {
+            while (scan.hasNext()) {
                 firstName = scan.next();
                 lastName = scan.next();
                 id = scan.next();
@@ -32,17 +31,10 @@ public class Company {
         } catch (IOException e) {
             System.out.println("File not found");
         }
+        return employees;
     }
 
-    public void printEmployees() {
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i] != null) {
-                System.out.println(employees[i]);
-            }
-        }
-    }
-
-    public double averageSalary(Employee[] employees) {
+    public static double averageSalary(Employee[] employees) {
         double sumSalary = 0;
         int employeesNumber = 0;
         for (int i = 0; i < employees.length; i++) {
@@ -54,7 +46,7 @@ public class Company {
         return sumSalary / employeesNumber;
     }
 
-    public double lowestSalary(Employee[] employees) {
+    public static double lowestSalary(Employee[] employees) {
         int lowestSalary = employees[0].getSalary();
         for (int i = 0; i < employees.length; i++) {
             if (employees[i] != null) {
@@ -66,7 +58,7 @@ public class Company {
         return lowestSalary;
     }
 
-    public double highestSalary(Employee[] employees) {
+    public static double highestSalary(Employee[] employees) {
         int highestSalary = employees[0].getSalary();
         for (int i = 0; i < employees.length; i++) {
             if (employees[i] != null) {
@@ -78,7 +70,7 @@ public class Company {
         return highestSalary;
     }
 
-    public String employeeOfDepartment(Employee[] employees) {
+    public static String employeeOfDepartment(Employee[] employees) {
         int itEmployee = 0;
         int supportEmployee = 0;
         int managementEmployee = 0;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,26 +5,24 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        Company company = new Company();
-        File file = new File("employees.csv");
+    public static void main(String[] args) {
+        File stats = new File("stats.txt");
 
-        if (file.exists()) {
-            File stats = new File("stats.txt");
-            stats.createNewFile();
-            company.readEmployees();
+        try (
+                FileWriter fileWriter = new FileWriter(stats);
+                BufferedWriter writer = new BufferedWriter(fileWriter);
+        ) {
+            Employee[] employees = Company.readEmployees();
 
-            FileWriter fileWriter = new FileWriter(stats);
-            BufferedWriter writer = new BufferedWriter(fileWriter);
-            writer.write("Średnia wypłata: " +  company.averageSalary(company.employees));
+            writer.write("Średnia wypłata: " + Company.averageSalary(employees));
             writer.newLine();
-            writer.write("Minimalna wypłata: " + company.lowestSalary(company.employees));
+            writer.write("Minimalna wypłata: " + Company.lowestSalary(employees));
             writer.newLine();
-            writer.write("Maksymalna wypłata: " + company.highestSalary(company.employees));
+            writer.write("Maksymalna wypłata: " + Company.highestSalary(employees));
             writer.newLine();
-            writer.write(company.employeeOfDepartment(company.employees));
-
-            writer.flush();
+            writer.write(Company.employeeOfDepartment(employees));
+        } catch (IOException e) {
+            System.err.println("Nie odnaleziono pliku");
         }
     }
 }
